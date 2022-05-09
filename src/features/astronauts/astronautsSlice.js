64561bd34589bf4b1e9e32843160fd2709f94713
shu Lin <bbqlinshu@gmail.com>
2022-05-08 20:27:14 -0400

    Completed

--- src/features/astronauts/astronautsSlice.js
+++ src/features/astronauts/astronautsSlice.js
@@ -1,14 +1,21 @@
 // Action Creators
-export function fetchAstronauts(astronauts) {
-  return {
-    type: "astronauts/astronautsLoaded",
-    payload: astronauts,
-  };
+export function fetchAstronauts() {
+  return function (dispatch) {
+    dispatch({type: "astronauts/astronautsLoading"})
+    fetch("http://api.open-notify.org/astros.json")
+    .then(r=>r.json())
+    .then(astronauts=>
+      dispatch({
+        type: "astronauts/astronautsLoaded",
+        payload: astronauts.people,
+      }))
+ };
 }
 
 // Reducers
 const initialState = {
   entities: [], //array of astronauts
+  status: "idle"
 };
 
 export default function reducer(state = initialState, action) {
@@ -18,7 +25,11 @@ export default function reducer(state = initialState, action) {
         ...state,
         entities: action.payload,
       };
-
+    case "astronauts/astronautsLoading":
+      return {
+        ...state,
+        status: "loading"
+      }
     default:
       return state;
   }
